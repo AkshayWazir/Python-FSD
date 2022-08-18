10b77277e315973ca55eaf4ecde17ca2f274e944
AkshayWazir <waizaeer4@gmail.com>
2022-08-18 16:24:34 +0530

    react-app

---
+++ reactFun/steps.js
@@ -0,0 +1,3 @@
+// * first move to your directory you want to create the app
+// * then write this command "npx create-react-app app-name" where "app-name" is the name of your application
+// ? once app is made. then write "cd app-name" and further write cd "npm start"
