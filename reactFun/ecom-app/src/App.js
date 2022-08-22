d5a32b93dfeef4890b3aec3c0c36db52c955aab1
AkshayWazir <waizaeer4@gmail.com>
2022-08-22 10:05:02 +0530

    Stable state

--- reactFun/ecom-app/src/App.js
+++ reactFun/ecom-app/src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import "./App.css";
-import Card from "./components/card";
+import { ShoppingDashboard } from "./screens";
 
 export default function App() {
-  const cards = [
-    { title: "Shoes", price: 200 },
-    { title: "Tshirt", price: 100 },
-    { title: "Pants", price: 400 },
-  ];
+  const renderPage = 1;
 
   return (
     <div className="App">
-      {cards.map((item) => (
-        <Card {...item} />
-      ))}
-      {/* <Card title="Shoes" price="100" />
-      <Card title="Pants" price="500" />
-      <Card title="Hats" price="400" /> */}
+      <ShoppingDashboard />
     </div>
   );
 }
---
+++ reactFun/ecom-app/src/screens/shoppingDashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TopNavBar } from "../../components/shared";
+import "./styles.css";
+
+export default function ShoppingDashboard(props) {
+  return (
+    <div className="shopDash-container">
+      <TopNavBar />
+    </div>
+  );
+}
