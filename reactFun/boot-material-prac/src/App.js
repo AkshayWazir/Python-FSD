fbaa57f9577087cc100d98b2f50b314ca09c35e0
AkshayWazir <waizaeer4@gmail.com>
2022-09-01 10:15:36 +0530

    Stabel state

---
+++ reactFun/boot-material-prac/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Dashboard } from "./screens";
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
