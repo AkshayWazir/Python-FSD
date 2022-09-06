293a1bda0ef3937bcde9c159f77a2cbdb4a7a9a7
AkshayWazir <waizaeer4@gmail.com>
2022-09-06 10:10:41 +0530

    restfull

--- flask/first-api/restfulSetup.py
+++ flask/first-api/restfulSetup.py
@@ -4,24 +4,46 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
-class Store(Resource):
-    def get(self, name):
-        return {'store-name': name}
-
-    def post(self):
-        body = request.get_json()
-        return {'name': 'Result'}
+stores = []
 
 
-class Car(Resource):
-    def get(self):
-        return {'name': 'tat'}
+class Item():
+    def __init__(self, name, _id, price) -> None:
+        self.id = _id
+        self.name = name
+        self.price = price
 
 
-api.add_resource(Store, '/store/<string:name>')
+class Store():
+    def __init__(self, name: str, _id: int) -> None:
+        self.name = name
+        self.id = _id
+        self.items = []
 
-api.add_resource(Car, '/car')
+    def addItem(self, item: Item):
+        foundItems = next(filter(lambda x: x.id == item.id), None)
+        if foundItems is not None:
+            return {'message': 'Item already exists'}
+        else:
+            self.items.append(item)
+
+
+class StoreAPI(Resource):
+    def get(self, id):
+        # TODO fetch the store here
+        pass
+
+    def post(self, id):
+        # TODO modify store here
+        pass
+
+    def put(self, id):
+        # TODO Add a new store if it does not exists
+        pass
+
+    def delete(self, id):
+        # TODO remove the existing store
+        pass
 
 
 app.run(debug=True, port=5000)
