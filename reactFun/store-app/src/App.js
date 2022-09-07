a14914150a74ba83ab4623890926e056bc3d60b0
AkshayWazir <waizaeer4@gmail.com>
2022-09-07 14:37:29 +0530

    stable state

--- reactFun/store-app/src/App.js
+++ reactFun/store-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import MakeRequest from "./utils/apiHandler";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async function apiCall() {
+    const result = await MakeRequest("http://127.0.0.1:5000/store/1").put({ name: "Sharma Store" });
+
+    console.log(result);
+  }
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
