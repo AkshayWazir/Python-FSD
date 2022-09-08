b8a3c08c82cf981d5046c283bf1c9a0da40e8c74
AkshayWazir <waizaeer4@gmail.com>
2022-09-08 14:17:55 +0530

    stable login system

--- reactFun/store-app/src/screens/loginSignup/index.jsx
+++ reactFun/store-app/src/screens/loginSignup/index.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import "./styles.css";
-import { sigupUserApiCall } from "./apis";
+import { sigupUserApiCall, loginUserAPIcall } from "./apis";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginSignupPage(props) {
   const [data, setData] = useState({ username: "", password: "" });
   const [status, setStatus] = useState("");
+  const navigate = useNavigate();
 
-  async function loginUser() {}
+  async function loginUser() {
+    let res = await loginUserAPIcall(data);
+    if (res.state) {
+      localStorage.setItem("token", res.data.data);
+      navigate("/dashboard");
+      setData({ username: "", password: "" });
+    } else {
+      setStatus("User not authorized", res.data);
+    }
+  }
 
   async function signupUser() {
     let res = await sigupUserApiCall(data);
