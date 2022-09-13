ba83bbbfac90bbbae7b0132fe31b36baf978eb28
AkshayWazir <waizaeer4@gmail.com>
2022-09-13 12:32:08 +0530

    stable

--- flask/dbconnect/connectDb.py
+++ flask/dbconnect/connectDb.py
@@ -25,3 +25,22 @@ myCursor.executemany(insertQuery, appUsers)
 
 myDb.commit()
 myDb.close()
+
+# ? Create Table
+# * To create database for a Clothing brand that sells all kinds of clothes
+# * We need one Database : sql_clothing
+# *     table name : columns required
+# *     sql_customers : customerId(int, autoIncrement) , fullName(varchar: 50, notNull), address, number, prime
+# *     sql_store : storeID, name, address, invId, locationCode
+# *     sql_inventory : prodId, name, description, Qty, price, locId
+# *     sql_location : locId, name
+
+# ? create Data
+# * Function 1 : toCreate New Customers (everything except customerId)
+# * func 2 : to Create a new Store : (everything except storeId)
+# * func 3 : to Create a new Inventory : (everything except prodId)
+# * func 4 : to creaete location with name (except locId)
+
+# ? Create Relation
+# * Foregin key between location code in sql_store and sql_location
+# * Foregin key between location code in prod_id and invId
