8e3be5cf2eb684887dc9b765f513d17b3055d993
AkshayWazir <waizaeer4@gmail.com>
2022-09-14 13:19:40 +0530

    result

---
+++ flask/store-backend/model/customerTable.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
+
+class Customers(db.Model):
+    __tablename__ = "sql_customers"
+    customerId = db.Column(db.Integer, primary_key=True, nullable=False)
+    fullName = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+    number = db.Column(db.String(10), nullable=False)
+    prime = db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.fullName
